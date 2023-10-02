5901622f2999de94bee9e343c4bf42c233074bed
Hendra Anggrian <hendraanggrian@gmail.com>
2023-10-02 15:59:21 -0500

    Move main to Lab1, add Lab2

--- Lab1/scripts/scale.fix.js
+++ Lab1/scripts/scale.fix.js
@@ -4,7 +4,8 @@ let i
 if (navigator.userAgent.match(/iPhone/i)) {
   for (i = 0; i < metas.length; i++) {
     if (metas[i].name === 'viewport') {
-      metas[i].content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0'
+      metas[i].content = 'width=device-width, minimum-scale=1.0, ' +
+          'maximum-scale=1.0'
     }
   }
   document.addEventListener('gesturestart', gestureStart, false)
@@ -13,7 +14,8 @@ if (navigator.userAgent.match(/iPhone/i)) {
 function gestureStart() {
   for (i = 0; i < metas.length; i++) {
     if (metas[i].name === 'viewport') {
-      metas[i].content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6'
+      metas[i].content = 'width=device-width, minimum-scale=0.25, ' +
+          'maximum-scale=1.6'
     }
   }
 }
